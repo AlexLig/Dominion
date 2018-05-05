c863119c17988de79bfa4ef2dae5262480fc4433
Alextl <alex.trl@hotmail.com>
2018-05-05 23:07:36 +0300

    README

--- src/main/java/dominion/Main.java
+++ src/main/java/dominion/Main.java
@@ -1,38 +1,11 @@
 package dominion;
 
-import dominion.game.Game;
-
 
 public class Main {
 
     public static void main(String[] args) {
 
-        //Testing game: dealing a new game with 4 players.
 
-        Game game = new Game(4);
-        System.out.println("Welcome to new Dominion with " + game.getNumberOfPlayers() + " players!");
-        game.newGame();
-
-        for (int i = 0; i < game.getNumberOfPlayers(); i++) {
-            System.out.println("Player " + i + ": ");
-            for (int j = 0; j < game.getListOfPlayers().get(i).handDeck.size(); j++) {
-                System.out.println(game.getListOfPlayers().get(i).handDeck.get(j).getName());
-
-            }
-
-
-
-        }
-        for (int i = 0; i < game.getNumberOfPlayers(); i++) {
-            System.out.println("Player " + i + ": ");
-            for (int j = 0; j < game.getListOfPlayers().get(i).handDeck.size(); j++) {
-                System.out.println(game.getListOfPlayers().get(i).handDeck.get(j).getName());
-
-            }
-
-
-
-        }
 
 
 
--- src/main/java/dominion/cards/Action.java
+++
@@ -1,8 +0,0 @@
-package dominion.cards;
-
-public class Action extends Card {
-
-    public Action(String name, String description, CardType cardType, int cost, int numberOfCopies) {
-        super(name, description, cardType, cost, numberOfCopies);
-    }
-}
